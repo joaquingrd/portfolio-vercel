2eddda8a2c2d8bd9c01ceddc25049070b2121a67
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-07-27 17:54:04 -0300

    agregué nuevos estilos para el modo dark en el componente footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,17 @@
 import "./Footer.css";
+import { useSelector } from "react-redux";
 
 const Footer = () => {
+  const mode = useSelector((state) => state.mode);
+
   return (
     <footer>
-      <div className="col-md-12 ">
-        <div className="copyright text-center">
+      <div className="col-md-12">
+        <div
+          className={`copyright text-center  ${
+            mode === "dark" && "copyright-dark"
+          }`}
+        >
           <div className="container pt-1">
             <small>Copyright &copy; Joaquin Ruiz Diaz</small>
           </div>
