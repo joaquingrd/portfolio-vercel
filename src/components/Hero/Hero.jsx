43283dcb7c92ee5569794249a475bf561cfa5cdf
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-07-27 16:45:14 -0300

    agregué el nuevo estilo dark en el componente hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,11 +6,15 @@ import CVp from "../../pdfs/CVp_Joaquin_RuizDiaz.pdf";
 import { Icon } from "@iconify/react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectLanguage } from "../../redux/actions";
+import { selectMode } from "../../redux/actions";
+
 import React, { useState } from "react";
 
 const Hero = () => {
   const [lastLanguage, setLastLanguage] = useState("es"); // estado local para recordar el último idioma seleccionado
   const language = useSelector((state) => state.language);
+  const [lastMode, setLastMode] = useState("dark"); // estado local para recordar el último idioma seleccionado
+  const mode = useSelector((state) => state.mode);
 
   const dispatch = useDispatch();
 
@@ -18,6 +22,12 @@ const Hero = () => {
     const newLanguage = lastLanguage === "es" ? "pt" : "es"; // cambia al último idioma seleccionado
     setLastLanguage(newLanguage); // guarda el último idioma seleccionado
     dispatch(selectLanguage(newLanguage));
+  };
+
+  const handleModeChange = () => {
+    const newMode = lastMode === "dark" ? "sun" : "dark"; // cambia al último idioma seleccionado
+    setLastMode(newMode); // guarda el último idioma seleccionado
+    dispatch(selectMode(newMode));
   };
 
   const heroData = useSelector((state) => state.data);
@@ -29,9 +39,15 @@ const Hero = () => {
           <div className="hero-text">
             <h1 className="hero-title">{heroData.title1}</h1>
             <h1 className="hero-title2">{heroData.title2}</h1>
-            <h2 className="hero-subtitle">{heroData.subtitle}</h2>
+            <h2
+              className={`hero-subtitle ${
+                mode === "dark" && "hero-subtitle-dark"
+              }`}
+            >
+              {heroData.subtitle}
+            </h2>
           </div>
-          <div className="descargar">
+          <div className={`descargar ${mode === "dark" && "descargar-dark"}`}>
             <a
               href={language === "es" ? CVe : CVp}
               target="_blank"
@@ -46,13 +62,24 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className="contenedor2  col-md-3">
+        <div
+          className={`col-md-3 ${
+            mode === "dark" ? "contenedor2-dark" : "contenedor2"
+          }`}
+        >
           <div className="language-btn-container">
             <button className="language-btn" onClick={handleLanguageChange}>
               {lastLanguage === "es" ? (
                 <Icon className="ic-language" icon="twemoji:flag-brazil" />
               ) : (
                 <Icon className="ic-language" icon="twemoji:flag-spain" />
+              )}
+            </button>
+            <button className="language-btn" onClick={handleModeChange}>
+              {lastMode === "dark" ? (
+                <Icon className="ic-dark" icon="twemoji:sun" />
+              ) : (
+                <Icon className="ic-dark" icon="solar:moon-bold" />
               )}
             </button>
           </div>
