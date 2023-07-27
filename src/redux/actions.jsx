2135e47bf21fd8820b6df56c3036a3cffca90a6b
Joaquin Ruiz Diaz <joaquing.ruizdiaz@gmail.com>
2023-07-27 16:43:27 -0300

    agregué la action del boton para el modo dark

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -2,11 +2,20 @@ import data_es from "../Data/data_es.json";
 import data_pt from "../Data/data_pt.json";
 
 export const SELECT_LANGUAGE = "SELECT_LANGUAGE";
+export const SELECT_MODE = "SELECT_MODE";
 
 export const selectLanguage = (language) => {
   const data = language === "es" ? data_es : data_pt;
   return {
     type: SELECT_LANGUAGE,
+    payload: data,
+  };
+};
+
+export const selectMode = (mode) => {
+  const data = mode === "dark" ? "dark" : "ligth";
+  return {
+    type: SELECT_MODE,
     payload: data,
   };
 };
